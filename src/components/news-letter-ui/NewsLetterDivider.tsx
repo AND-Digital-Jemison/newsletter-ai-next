a0f0d294b304444b44a738f170ce4c05566589e7
robertwilkinson <robert@robertwilkinson.dev>
2025-07-07 16:57:40 +0100

    remove unused draggable components and related types

--- src/components/news-letter-ui/NewsLetterDivider.tsx
+++
@@ -1,9 +0,0 @@
-import { Divider } from '@mantine/core';
-import { FC } from 'react';
-import { NewsLetterComponent } from './types';
-
-type NewsLetterDividerProps = NewsLetterComponent;
-
-export const NewsLetterDivider: FC<NewsLetterDividerProps> = () => {
-  return <Divider my='lg' />;
-}
--- src/components/news-letter-ui/types.ts
+++
@@ -1,5 +0,0 @@
-import { RefObject } from 'react';
-
-export type NewsLetterComponent = {
-    // ref: RefObject<HTMLDivElement | null>
-}
